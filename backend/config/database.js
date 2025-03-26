b57a38b6a73f473cec35bf26e4c68aaaa111363f
Ubuntu <malkisarapiyumi918@gmail.com>
2025-03-26 12:11:03 +0000

    scraper

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,11 +2,16 @@ const { Pool } = require('pg');
 
 // PostgreSQL connection configuration
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'aivtdb',
-  password: 'admin',
-  port: 5432
+  // user: 'postgres',
+  // host: 'localhost',
+  // database: 'aivtdb',
+  // password: 'admin',
+  // port: 5432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 
 // Test the database connection
