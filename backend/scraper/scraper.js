287b5394f280efa94fc45b775058f8e1a1b6ae30
Ubuntu <malkisarapiyumi918@gmail.com>
2025-03-26 12:11:03 +0000

    scraper

---
+++ backend/scraper/scraper.js
@@ -0,0 +1,19 @@
+
+const axios = require('axios');
+const pool = require('../config/database');
+
+
+async function fetchCVEData(cveId) {
+    try {
+        const response = await axios.get(`https://cveawg.mitre.org/api/cve/${cveId}`);
+        // console.log("CVE Data from MITRE:", response.data);
+        // console.log('Description: ', response.data.containers.cna.descriptions);
+        console.log('Descriptions: ', response.data.containers.cna.descriptions.map(desc => desc.value));
+    } catch (error) {
+        console.error("Error fetching CVE data from MITRE:", error);
+    }    
+}
+
+fetchCVEData("CVE-2025-24666");
+
+
